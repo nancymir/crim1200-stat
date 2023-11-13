3ab25228690b7338afe1d2d58ada4f9189d6c99f
nancymir <123514863+nancymir@users.noreply.github.com>
2023-11-13 13:48:08 -0500

    uploaded exam 2

---
+++ Exam 2.R
@@ -0,0 +1,42 @@
+
+# 1. Data
+
+# a. Load the data and preview it. How many individuals' responses were included?
+
+
+
+# 2. EDA single variable: Do EDA, visual and quantitative, and write down in words what are some surprising features of each variable.
+
+# a. Describe citizen.complaints visually and quantitatively
+
+# b. Describe reports.filed.by.officer visually and quantitatively
+
+# c. Describe bwc visually and quantitatively
+
+
+
+
+# 3. Comparing a quantitative variable and a categorical variable.
+
+# a. EDA two variables: Describe the relationship between the two variables visually and quantitatively. (Remember to include categorical variables in ggplot as "factor(variable)" instead of just "variable".)
+# citizen.complaints and bwc
+
+# b. Statistical test: Run a t-test to test whether there are more citizen complaints when police officers wear a camera than when they do not. What are the null and alternative hypotheses? Is the p-value < 0.05? What can you conclude from this test?
+
+# c. Does this mean that if we give all police officers a body-worn camera, they will have a better relationship with the community?
+
+# d. Repeat the last three points comparing reports.filed.by.officer and bwc. What can you conclude here?
+
+
+
+# 4. Comparing two quantitative variables.
+
+# a. EDA two variables: Describe the relationship between citizen.complaints (y) and reports.filed.by.officer (x).
+
+# b. Fit a linear model regressing citizen.complaints (y) onto reports.filed.by.officer (x). Are the assumptions of a linear model satisfied? If so, move on to the next problem. If not, try transforming the data.
+
+# c. Interpret the slope coefficient. What can you say about the relationship between citizen.complaints (y) and reports.filed.by.officer (x)? Do we see more citizen complaints for officers that produce more reports against citizens? 
+
+# d. What does the confidence interval on the slope coefficient tell us?
+
+
